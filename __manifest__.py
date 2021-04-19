bff59344dda02aea30fc75e40c9bfb64fecd1e33
Ignacio Buioli <ibuioli@gmail.com>
2021-04-18 22:21:45 -0300

    feat: initial commit

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Website Product Multiwebsite',
+    'summary': 'Share products between websites',
+    'category': 'Website',
+    'version': '13.0.1.0.0',
+    'author': 'Trey, Moldeo Interactive',
+    'website': '',
+    'license': 'AGPL-3',
+    'depends': ['website_sale'],
+    'data': [
+        'views/product_template.xml'
+    ],
+    'installable': True,
+}
